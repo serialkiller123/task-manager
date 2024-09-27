bc5b3ddbd525a4326b89033a0df55ad5fc079e31
serialkiller123 <inaax12@gmail.com>
2024-09-27 18:08:28 +0500

    build updated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient;
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
 }
 
 export default prisma;
